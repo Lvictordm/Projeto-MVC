10d534a73ec2bf792270f882833113fa15c07831
lohan <lohanvictorlimaaa@gmail.com>
2026-06-01 12:44:51 -0300

    adicionando controlador de movimentações com rotas e templates, incluindo registro e histórico de movimentações

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,3 +2,8 @@
 from app.models import categoria
 from app.models import produtos
 from app.models import usuario
+from app.models import movimentacao
+
+#gerar a migration
+#python -m alembic revision --autogenerate -m "Criando as tabelas do banco de dados"
+#python -m alembic upgrade head
