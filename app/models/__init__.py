1939cfac86d0af213f55fb0bf2e64e8c59fe6e69
lohan <lohanvictorlimaaa@gmail.com>
2026-06-02 11:23:13 -0300

    adicionando pdv clientes e venda com todas as funções

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,9 +1,16 @@
+#gerar a migration
+#python -m alembic revision --autogenerate -m "Criando as tabelas do banco de dados"
+#python -m alembic upgrade head
+
 
 from app.models import categoria
 from app.models import produtos
 from app.models import usuario
 from app.models import movimentacao
+from app.models import cliente
+from app.models import venda
 
-#gerar a migration
-#python -m alembic revision --autogenerate -m "Criando as tabelas do banco de dados"
-#python -m alembic upgrade head
+
+# python -m alembic revision --autogenerate -m "Criar tabelas cliente e vendas"
+
+# python -m alembic upgrade head
